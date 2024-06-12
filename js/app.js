7c43c89413549d7024c89324e4644b14df5a2489
dwqkwqdwqodpk <kapateli2019@gmail.com>
2024-06-12 11:58:23 +0300

    upd

--- js/app.js
+++ js/app.js
@@ -12,3 +12,47 @@ mobileClose.onclick = function () {
 	mobileOpen.classList.toggle('none')
 	body.classList.toggle('no-scroll')
 }
+// swiper
+
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	autoplay: {
+		enabled: true,
+		speed: 3000,
+	},
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+})
+
+// calendar
+const currentDate = document.querySelector('.excursions__calendar-title')
+
+let date = new Date(),
+	currYear = date.getFullYear(),
+	currMonth = date.getMonth()
+
+const months = [
+	'Январь',
+	'Февраль',
+	'Март',
+	'Апрель',
+	'Май',
+	'Июнь',
+	'Июль',
+	'Август',
+	'Сентябрь',
+	'Октябрь',
+	'Ноябрь',
+	'Декабрь',
+]
+
+const renderCalendar = () => {
+	currentDate.innetText = `${months[currMonth]}`
+	let lastDayofMOnth = new Date(currYear, currMonth + 1, 0).getDate()
+}
+
+renderCalendar()
